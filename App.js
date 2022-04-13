a2ca12f788b18bd2c1c9e134560c971ea722d184
Jose Luis Manon <manometho@gmail.com>
2022-04-12 23:04:33 -0400

    Actualizo los fonts del proyecto

--- App.js
+++ App.js
@@ -5,9 +5,7 @@ import { useFonts } from 'expo-font';
 export default function App() {
   
   const [loaded] = useFonts({
-      'regular':require('./assets/Fonts/Rajdhani-Regular.ttf'),
       'bold': require('./assets/Fonts/Rajdhani-Bold.ttf'),
-      'light':require('./assets/Fonts/Rajdhani-Light.ttf'),
       'medium':require('./assets/Fonts/Rajdhani-Medium.ttf'),
       'semiBold':require('./assets/Fonts/Rajdhani-SemiBold.ttf')
   });
@@ -23,7 +21,16 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Text><Text style={styles.bell}>Bell</Text><Text style={styles.dr}>DR</Text> React Native App</Text>
+      <Text>
+        <Text style={styles.bell}>Bell</Text>
+        <Text style={styles.dr}>DR </Text>
+        <Text style={styles.bodyText}>React Native App</Text>
+      </Text>
+
+      <Text style={styles.title}>Titulo del Articulo</Text>
+      <Text style={styles.subtitle}>Sub-Titulo del Articulo</Text>
+      <Text style={styles.content}>Delightful Onboarding for your React-Native app. Let your users get familiar with your app by giving a pleasurable introduction. The Onboarding component is easy to set up and but still customizable.</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -32,19 +39,38 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ECECEC',
     alignItems: 'center',
     justifyContent: 'center',
+    
   },
   bell:{
     fontFamily:'bold',
-    fontSize:30,
+    fontSize:35,
     color:"#00930A",
   },
   dr:{
     fontFamily:'medium',
-    fontSize:30,
+    fontSize:35,
     color:"#006472",
     fontWeight:'800'
   },
+  bodyText:{
+    fontFamily:'semiBold',
+    fontSize:18,
+  },
+  title:{
+    fontFamily:'bold',
+    fontSize:25,
+  },
+  subtitle:{
+    fontFamily:'semiBold',
+    fontSize:20,
+  },
+  content:{
+    fontFamily:'medium',
+    fontSize:16,
+
+  }
+
 });
